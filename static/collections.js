4f38627875358f1a229f512f4dcd2591ec29b815
Shlok Kumar <shlokkumar2303@gmail.com>
2022-06-01 13:08:54 +0530

    finished

---
+++ static/collections.js
@@ -0,0 +1,22 @@
+export const doodlesCollection = {
+  name: 'OtherDeed by OtherSide',
+  creator: 'OtherSide_LLC',
+  description:
+    'Otherdeed is the key to claiming land in Otherside. Each have a unique blend of environment and sediment — some with resources, some home to powerful artifacts. And on a very few, a Koda roams.',
+  isVerified: true,
+  slug: 'otherside-official',
+  banner_image_url:"/hero-banner.png",
+  featured_image_url:
+    'https://lh3.googleusercontent.com/svc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw=s300',
+  image_url:"/otherside-creator.jpeg",
+  discord_url: 'https://discord.gg/doodles',
+  external_url: 'https://doodles.app',
+  twitter_username: 'doodles', 
+  stats: {
+    total_volume: 119183.29519312919,
+    total_supply: 10000,
+    num_owners: 4897,
+    floor_price: 22.6,
+  },
+  address: '0x8a90cab2b38dba80c64b7734e58ee1db38b8992e',
+}
