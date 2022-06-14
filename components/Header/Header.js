9ab1e5c83233dd88b3f530da8e4fd50a05cf82c5
Shlok Kumar <shlokkumar2303@gmail.com>
2022-06-14 17:47:06 +0530

    update v2

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,99 +1,97 @@
-import { useState, useEffect } from 'react'
-import Logo from './Logo'
+import { useState, useEffect } from "react";
+import Logo from "./Logo";
 import {
-  SearchIcon,
-  MenuIcon,
-  CreditCardIcon,
-  MoonIcon,
-  SunIcon,
-} from '@heroicons/react/outline'
-import { UserCircleIcon } from '@heroicons/react/solid'
-import SearchInput from './SearchInput'
-import NavMenus from './NavMenus'
-import Link from 'next/link'
-import { useTheme } from 'next-themes'
+  HiOutlineSun,
+  HiOutlineSearch,
+  HiOutlineMoon,
+  HiOutlineCreditCard,
+  HiOutlineLogout,
+} from "react-icons/hi";
+import SearchInput from "./SearchInput";
+import NavMenus from "./NavMenus";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useAddress, useDisconnect } from "@thirdweb-dev/react";
 
 function Navbar() {
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
+  const address = useAddress();
+  const disconnect = useDisconnect();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  const { systemTheme, theme, setTheme } = useTheme()
+  const { systemTheme, theme, setTheme } = useTheme();
 
   const renderThemeChanger = () => {
-    if (!mounted) return null
+    if (!mounted) return null;
 
-    const currentTheme = theme === 'system' ? systemTheme : theme
+    const currentTheme = theme === "system" ? systemTheme : theme;
 
-    if (currentTheme === 'dark') {
+    if (currentTheme === "dark") {
       return (
-        <MoonIcon
-          className='h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300'
-          role='button'
-          onClick={() => setTheme('light')}
+        <HiOutlineMoon
+          className="h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300"
+          role="button"
+          onClick={() => setTheme("light")}
         />
-      )
+      );
     } else {
       return (
-        <SunIcon
-          className='h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300'
-          role='button'
-          onClick={() => setTheme('dark')}
+        <HiOutlineSun
+          className="h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300"
+          role="button"
+          onClick={() => setTheme("dark")}
         />
-      )
+      );
     }
-  }
+  };
 
   const menus = [
     {
-      name: 'Etherscan',
+      name: "Etherscan",
       href: `https://rinkeby.etherscan.io/address/${process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT_ADDRESS}`,
     },
     {
-      name: 'OpenSea',
-      href: 'https://testnets.opensea.io/collection/otherdeed-ce8y1tjrgw',
+      name: "OpenSea",
+      href: "https://testnets.opensea.io/collection/otherdeed-ce8y1tjrgw",
     },
-    {
-      name: 'Resources',
-      href: '#',
-    },
-    {
-      name: 'Create',
-      href: '#',
-    },
-  ]
+  ];
 
   return (
-    <header className='sticky top-0 z-50 bg-white px-4 py-2 shadow-md dark:bg-gray-900'>
-      <div className='flex items-center justify-between space-x-6'>
-        <div className='xl:pr-40'>
-          <Link href='/'>
+    <header className="sticky top-0 z-50 bg-white px-4 py-2 shadow-md dark:bg-gray-900">
+      <div className="flex items-center justify-between space-x-6">
+        <div className="xl:pr-40">
+          <Link href="/">
             <a>
               <Logo />
             </a>
           </Link>
         </div>
 
-        <div className='ml-8 hidden flex-1 sm:block'>
+        <div className="ml-8 hidden flex-1 sm:block">
           <SearchInput />
         </div>
 
-        <div className='hidden pr-6 lg:block xl:pl-8'>
+        <div className="hidden pr-6 lg:block xl:pl-8">
           <NavMenus menus={menus} />
         </div>
 
-        <div className='flex items-center space-x-6'>
-          <UserCircleIcon className='hidden h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white lg:block' />
-          <CreditCardIcon className='hidden h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white lg:block' />
+        <div className="flex items-center space-x-6">
+          <Link href={`https://testnets.opensea.io/${address}`}>
+            <HiOutlineCreditCard className="h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white" />
+          </Link>
           {renderThemeChanger()}
-          <SearchIcon className='h-7 w-7 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white sm:hidden' />
-          <MenuIcon className='h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white lg:hidden' />
+          <HiOutlineSearch className="h-7 w-7 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white sm:hidden" />
+          <HiOutlineLogout
+            className="h-8 w-8 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300 hover:dark:text-white"
+            onClick={disconnect}
+          />
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- components/Header/SearchInput.js
+++ components/Header/SearchInput.js
@@ -1,18 +1,18 @@
-import { SearchIcon } from '@heroicons/react/outline'
+import { HiOutlineSearch } from "react-icons/hi";
 
 function SearchInput() {
   return (
-    <div className='rounded-lg border px-1 py-2 focus:shadow-md dark:border-transparent dark:bg-[#333333]'>
-      <div className='flex items-center space-x-3 pl-3'>
-        <SearchIcon className='h-5 w-5 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300' />
+    <div className="rounded-lg border px-1 py-2 focus:shadow-md dark:border-transparent dark:bg-[#333333]">
+      <div className="flex items-center space-x-3 pl-3">
+        <HiOutlineSearch className="h-5 w-5 cursor-pointer text-gray-600 transition-all hover:text-black dark:text-gray-300" />
         <input
-          className='w-full bg-transparent text-gray-600 outline-none placeholder:text-sm dark:text-gray-300 md:placeholder:text-base'
-          type='text'
-          placeholder='Search items, collections, and accounts'
+          className="w-full bg-transparent text-gray-600 outline-none placeholder:text-sm dark:text-gray-300 md:placeholder:text-base"
+          type="text"
+          placeholder="Search items, collections, and accounts"
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default SearchInput
+export default SearchInput;
